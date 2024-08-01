27e7a80f3af304262d73f4e7c2e8cf3ba58582e5
ocebert31 <153503666+ocebert31@users.noreply.github.com>
2024-08-01 16:43:38 +0200

    modification  du share

--- src/Articles/ArticleShare/articleShare.js
+++ src/Articles/ArticleShare/articleShare.js
@@ -1,45 +1,40 @@
 import React from 'react';
 import { FaTwitter } from 'react-icons/fa';
-import { Helmet } from 'react-helmet';
 
 const Article = ({ article }) => {
+    const url= process.env.REACT_APP_URL
+
   const stripHtml = (html) => {
     const doc = new DOMParser().parseFromString(html, 'text/html');
     return doc.body.textContent || "";
   };
 
+  const truncateText = (text, maxLength) => {
+    if (text.length > maxLength) {
+      return text.slice(0, maxLength) + '...';
+    }
+    return text;
+  };
+
   const shareOnTwitter = () => {
     const plainTextContent = stripHtml(article.content);
-    const tweetText = `${article.title}\n\n${plainTextContent}`;
-    const twitterShareUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}&image=${encodeURIComponent(article.imageUrl)}`;
+    const truncatedContent = truncateText(plainTextContent, 100);
+    const twitterShareUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+        article.title + '\n\n' + 
+        truncatedContent + '\n\n' + 
+        `Lien de l'article: ${url}/articles/${article._id}`
+    )}`;      
     window.open(twitterShareUrl, '_blank', 'noopener,noreferrer');
   };
 
+
   return (
-    <>
-      <Helmet>
-        <title>{article.title}</title>
-        <meta name="title" content={article.title} />
-        <meta name="description" content={article.content} />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={`https://inkstream.alwaysdata.net/articles/${article._id}`} />
-        <meta property="og:title" content={article.title} />
-        <meta property="og:description" content={article.content} />
-        <meta property="og:image" content={article.imageUrl} />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:url" content={`https://inkstream.alwaysdata.net/articles/${article._id}`} />
-        <meta name="twitter:title" content={article.title} />
-        <meta name="twitter:description" content={article.content} />
-        <meta name="twitter:image" content={article.imageUrl} />
-      </Helmet>
-      <div>
-        <button onClick={shareOnTwitter}>
-          <FaTwitter className="text-2xl sm:text-3xl md:text-4xl" />
-        </button>
-      </div>
-    </>
+    <div>
+     
+      <button onClick={shareOnTwitter}>
+        <FaTwitter className="text-2xl sm:text-3xl md:text-4xl" />
+      </button>
+    </div>
   );
 };
 
