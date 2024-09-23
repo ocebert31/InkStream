e30f844dcca319339c913a2289176381e633b113
ocebert31 <153503666+ocebert31@users.noreply.github.com>
2024-09-23 15:27:00 +0200

    modify search in dashboard and add user number

--- src/Dashboard/dashboard.js
+++ src/Dashboard/dashboard.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { getUsers, updateUserRole } from '../API/admin'; 
 import { useAuth } from "../AuthContext";
 import InfiniteScrollComponent from '../Components/infiniteScroll';
+import Search from '../Articles/Search/search';
 
 function UsersList() {
     const [users, setUsers] = useState([]);
@@ -11,17 +12,19 @@ function UsersList() {
     const [page, setPage] = useState(1); 
     const [limit] = useState(10);
     const [hasMore, setHasMore] = useState(true);
+    const [totalUsers, setTotalUsers] = useState(0);
 
     useEffect(() => {
         if (token) {
             const loadUsers = async () => {
                 try {
-                    const fetchedUsers = await getUsers(searchQuery, page, limit, token);
+                    const fetchedUsers = await getUsers(searchQuery, page, limit, totalUsers, token);
                     setUsers(prevUsers => page === 1 ? fetchedUsers.users : [...prevUsers, ...fetchedUsers.users]);
 
                     if (fetchedUsers.users.length < limit) {
                         setHasMore(false);
                     }
+                    setTotalUsers(fetchedUsers.totalUsers)
                 } catch (error) {
                     setShowErrorAlert(true);
                 }
@@ -54,15 +57,17 @@ function UsersList() {
         return <div className="flex justify-center items-center h-screen text-red-500">Erreur lors du chargement des utilisateurs.</div>;
     }
 
-    if (users.length === 0) {
-        return <div className="flex justify-center items-center h-screen text-gray-500">Chargement...</div>;
+    const handleSearchQueryChange = (search) => {
+        setSearchQuery(search);
+        setPage(1);
     }
 
     return (
         <div className='bg-gray-100 min-h-screen font-montserrat'>
             <div className="container mx-auto px-4 py-8">
-                <input type="text" placeholder="Rechercher un utilisateur" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} className="border border-gray-300 p-2 mb-4 rounded-md w-full max-w-md"/>
+                <Search handleSearchQueryChange={handleSearchQueryChange} />
                 <InfiniteScrollComponent loadMore={() => setPage(page + 1)} dataLength={users.length} hasMore={hasMore}>
+                <p className="text-gray-800 font-bold mb-4">Total Users: {totalUsers}</p>
                     <ul>
                         {users.map(user => (
                             <div key={user._id} className="border border-gray-300 p-4 mb-4 flex justify-between items-center bg-white rounded-md shadow-md">
