dd75885c63b019736f6b75f1b2c3c47bae635f5c
ocebert31 <153503666+ocebert31@users.noreply.github.com>
2024-09-24 17:58:23 +0200

    refacto fetch categories

--- src/API/categories.js
+++ src/API/categories.js
@@ -1,79 +1,19 @@
-const url = process.env.REACT_APP_API_URL;
+import { fetchRequest } from "./fetchRequest";
 
 async function createCategories(data, token) {
-    try {
-        const response = await fetch(`${url}/categories`, {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {'Authorization': `Bearer ${token}`, 
-            'Content-Type': 'application/json'}
-        });
-        if (!response.ok) {
-            const error = await response.json();
-            throw new Error(error.message);
-        }
-        return await response.json();
-    } catch (error) {
-        console.error(error.message);
-        throw error;
-    }
+    return fetchRequest(`/categories`, { method: 'POST', body: data, token });
 }
 
 async function getCategories() {
-    try {
-        const response = await fetch(`${url}/categories`, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json; charset=UTF-8',
-                
-            }
-        });
-        if (!response.ok) {
-            const error = await response.json();
-            throw new Error(error.message);
-        }
-        return await response.json();
-    } catch (error) {
-        console.error(error.message);
-        throw error;
-    }
+    return fetchRequest(`/categories`, { method: 'GET' });
 }
 
 async function deleteCategory(id, token) {
-    try {
-        const response = await fetch(`${url}/categories/${id}`, {
-            method: 'DELETE',
-            headers: {'Authorization': `Bearer ${token}`}
-        });
-        if (!response.ok) {
-            const error = await response.json();
-            throw new Error(error.message);
-        }
-        return await response.json();
-    } catch (error) {
-        throw error;
-    }
-};
+    return fetchRequest(`/categories/${id}`, { method: 'DELETE', token });
+}
 
-async function updateCategory (id, name, token) {
-    try {
-        const response = await fetch(`${url}/categories/${id}`, {
-            method: 'PUT',
-            body: JSON.stringify({name}),
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            }
-        });
-        if (!response.ok) {
-            const error = await response.json();
-            throw new Error(error.message);
-        }
-
-        return await response.json();
-    } catch (error) {
-        throw error;
-    }
-};
+async function updateCategory(id, name, token) {
+    return fetchRequest(`/categories/${id}`, { method: 'PUT', body: {name}, token });
+}
 
 export {createCategories, getCategories, deleteCategory, updateCategory}
