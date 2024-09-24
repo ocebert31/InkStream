bd27102f39df8ddd31d19eac561c3e9385ac85d3
ocebert31 <153503666+ocebert31@users.noreply.github.com>
2024-09-24 17:23:56 +0200

    refacto fetch admin

--- src/API/admin.js
+++ src/API/admin.js
@@ -1,43 +1,12 @@
-const url = process.env.REACT_APP_API_URL;
+import { fetchRequest } from "./fetchRequest";
 
 async function getUsers(searchQuery = '', page = 1, limit = 10, token = null) {
-    const params = new URLSearchParams({ searchQuery, page, limit }).toString();
-    const urlGetUsers = `${url}/admin?${params}`;
-
-    try {
-        const response = await fetch(urlGetUsers, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json; charset=UTF-8',
-                ...(token && { 'Authorization': `Bearer ${token}` })
-            }
-        });
-        if (!response.ok) {
-            const error = await response.json();
-            throw new Error(error.message);
-        }
-        return await response.json();
-    } catch (error) {
-        console.error(error.message);
-        throw error;
-    }
+    const params = new URLSearchParams({ searchQuery, page, limit}).toString();
+    return fetchRequest(`/admin?${params}`, { method: 'GET', token });
 }
 
-async function updateUserRole (userId, newRole, token) {
-    const response = await fetch(`${url}/admin/${userId}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({ role: newRole }),
-    });
-
-    if (!response.ok) {
-        throw new Error('Erreur lors de la mise à jour du rôle');
-    }
-
-    return await response.json();
-};
+async function updateUserRole(userId, newRole, token) {
+    return fetchRequest(`/admin/${userId}`, { method: 'PUT', body: {role: newRole}, token });
+}
 
 export { getUsers, updateUserRole };
